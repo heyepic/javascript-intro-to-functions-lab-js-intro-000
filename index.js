c2001eed0de1d5bcbbbb2ee392e577f36c335339
Matt Bohlman <mattbohlman@gmail.com>
2020-05-20 17:30:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+function sayHiToGrandma(string) {
+  if(lowercase(string)) {
+    return "I can't hear you!"
+  }
+}
