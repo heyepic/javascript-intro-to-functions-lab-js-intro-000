8ad5e35de983cd3852d5a52d2e809773f82277ef
Matt Bohlman <mattbohlman@gmail.com>
2020-05-20 17:58:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,17 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
-  var uppercase = "HELLO!"
-  var lowercase = "hello!"
-  var mixedCase = "Hi there!"
-  if(string === lowercase) {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!"
-  } else if (string === uppercase) {
+  }
+
+  if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
 }
